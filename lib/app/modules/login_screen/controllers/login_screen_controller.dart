0ba29367a5cc76249548b2738238ed372926f4ba
Nitin Singh <i.nitinsingh18@gmail.com>
2023-03-14 00:24:34 +0530

    Login Screen

---
+++ lib/app/modules/login_screen/controllers/login_screen_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class LoginScreenController extends GetxController {
+  final TextEditingController smartId = TextEditingController();
+}
